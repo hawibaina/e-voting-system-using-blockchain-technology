114f9fc672786b941c9bc3a50158d43b260db923
hawibaina <hawibaina@gmail.com>
2023-09-20 17:32:22 +0300

    Initial Commit

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    "process.env.NODE_DEBUG": "''",
+  },
+
+  server:{
+    port: 3000
+  },
+  commonjsOptions: {
+    esmExternals: true,
+ },
+});
